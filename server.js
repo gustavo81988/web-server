ed8fdc9b957b2138c6704673d06ed10ac53c15e4
Gustavo Ramírez <gustavo81988@gmail.com>
2016-11-10 01:16:29 -0400

    Hello express

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app     = express();
+
+app.get('/',function(req, res){
+    res.send('Hello express');
+});
+
+app.get('/about',function(req, res){
+    res.send('About us');
+});
+
+app.listen(3000);
