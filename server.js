9c498ec9085477a72bcea042f13b171dd46509e1
Gustavo Ramírez <gustavo81988@gmail.com>
2016-11-10 01:50:21 -0400

    Middleware Is Awesome

--- server.js
+++ server.js
@@ -2,12 +2,24 @@ var express = require('express');
 var app     = express();
 var PORT    = 3000;
 
-app.get('/about',function(req, res){
+var middleware = {
+    requireAuthentication: function(req, res, next){
+        console.log('Private route hit!');
+        next();
+    },
+    logger: function(req, res, next){
+        console.log('Request: ' + new Date().toString() +' '+ req.method + ' ' + req.originalUrl);
+        next();
+    }
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
     res.send('About us');
 });
 
 app.use(express.static(__dirname + '/public'));
-
 
 app.listen(PORT,function(){
     console.log('Express server started on port '+ PORT +' !');
